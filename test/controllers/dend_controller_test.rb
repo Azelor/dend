fd6a9b3b30e76ee5c44964cb9a5d18ba9469c094
Azelor <kaarel.soot@gmail.com>
2015-12-28 19:02:03 +0200

    Add embedded ruby titles

--- test/controllers/dend_controller_test.rb
+++ test/controllers/dend_controller_test.rb
@@ -4,42 +4,42 @@ class DendControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "D&D - Home"
+    assert_select "title", "Home | D&D"
   end
 
   test "should get timeline" do
     get :timeline
     assert_response :success
-    assert_select "title", "D&D - Timeline"
+    assert_select "title", "Timeline | D&D"
   end
 
   test "should get history" do
     get :history
     assert_response :success
-    assert_select "title", "D&D - History"
+    assert_select "title", "History | D&D"
   end
 
   test "should get map" do
     get :map
     assert_response :success
-    assert_select "title", "D&D - Map"
+    assert_select "title", "Map | D&D"
   end
 
   test "should get stuff" do
     get :stuff
     assert_response :success
-    assert_select "title", "D&D - Stuff"
+    assert_select "title", "Stuff | D&D"
   end
 
   test "should get team" do
     get :team
     assert_response :success
-    assert_select "title", "D&D - Team"
+    assert_select "title", "Team | D&D"
   end
 
   test "should get factions" do
     get :factions
     assert_response :success
-    assert_select "title", "D&D - Factions"
+    assert_select "title", "Factions | D&D"
   end
 end
